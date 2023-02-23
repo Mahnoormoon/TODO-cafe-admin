cd97871a2c076940475da605b6cdcf9a2c9fa530
Mahnoormoon <mahnoormoon530@gmail.com>
2023-02-23 09:58:23 +0530

    changes

--- src/views/dashboard/Default/chart-data/total-growth-bar-chart.js
+++ src/views/dashboard/Default/chart-data/total-growth-bar-chart.js
@@ -67,15 +67,15 @@ const chartData = {
     },
     series: [
         {
-            name: 'Investment',
+            name: 'Total Users',
             data: [35, 125, 35, 35, 35, 80, 35, 20, 35, 45, 15, 75]
         },
         {
-            name: 'Loss',
+            name: 'Logouts',
             data: [35, 15, 15, 35, 65, 40, 80, 25, 15, 85, 25, 75]
         },
         {
-            name: 'Profit',
+            name: 'Total Growth',
             data: [35, 145, 35, 35, 20, 105, 100, 10, 65, 45, 30, 10]
         },
         {
